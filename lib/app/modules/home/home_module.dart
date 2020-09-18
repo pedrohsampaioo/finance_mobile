f74bbdbd3b4edd11332c337ac70ca7803576cf74
pedrohsampaio <pedrohsampaioo@outlook.com>
2020-09-18 12:12:38 -0300

    feat: dependency injection of created repositories and cubits

--- lib/app/modules/home/home_module.dart
+++ lib/app/modules/home/home_module.dart
@@ -1,10 +1,22 @@
 import 'package:flutter_modular/flutter_modular.dart';
 
+import '../../core/api/fake_api_client.dart';
+import '../../shared/fixtures/fixture_reader.dart';
+import 'cubits/transaction/transaction_cubit.dart';
+import 'cubits/user/user_cubit.dart';
 import 'home_page.dart';
+import 'repositories/transaction_repository.dart';
+import 'repositories/user_repository.dart';
 
 class HomeModule extends ChildModule {
   @override
-  List<Bind> get binds => [];
+  List<Bind> get binds => [
+        Bind((i) => UserRepository(FakeApiClient(FixtureReader.user))),
+        Bind((i) =>
+            TransactionRepository(FakeApiClient(FixtureReader.transactions))),
+        Bind((i) => TransactionCubit(i.get<TransactionRepository>())),
+        Bind((i) => UserCubit(i.get<UserRepository>())),
+      ];
 
   @override
   List<ModularRouter> get routers => [
