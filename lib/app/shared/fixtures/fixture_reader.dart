4ca1d5638ba517ee76c0aa8db20e34f2caf3eacd
pedrohsampaio <pedrohsampaioo@outlook.com>
2020-09-18 12:04:18 -0300

    feat: fixture to mock data

---
+++ lib/app/shared/fixtures/fixture_reader.dart
@@ -0,0 +1,36 @@
+class FixtureReader {
+  static Map<String, dynamic> get transactions => <String, dynamic>{
+        "data": [
+          {
+            "svg_path": "assets/svg/arrow_down.svg",
+            "subtitle": "Sending Payment to Clients",
+            "title": "Sent",
+            "value": 150
+          },
+          {
+            "svg_path": "assets/svg/arrow_down.svg",
+            "subtitle": "Sending Payment to Clients",
+            "title": "Sent",
+            "value": 150
+          },
+          {
+            "svg_path": "assets/svg/arrow_down.svg",
+            "subtitle": "Sending Payment to Clients",
+            "title": "Sent",
+            "value": 150
+          }
+        ]
+      };
+
+  static Map<String, dynamic> get user => <String, dynamic>{
+        "data": {
+          "image_url":
+              "https: //lifestyle.uai.com.br/wp-content/uploads/sites/9/2019/12/46.jpg",
+          "name": "Hira Riaz",
+          "career": "UX/UI Designer",
+          "expenses": 8900,
+          "income": 5500,
+          "loan": 890
+        }
+      };
+}
